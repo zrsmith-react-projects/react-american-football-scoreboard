0c380d11ae287eaf4c1813216a1dd3ff95ca6251
Zac Smith <zrs3141592@gmail.com>
2019-09-09 16:37:24 -0600

    add 15 sec countdown

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 //TODO: STEP 1 - Import the useState hook.
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
 
@@ -7,6 +7,17 @@ function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [homeScore, setHomeScore] = useState(0);
   const [awayScore, setAwayScore] = useState(0);
+
+  const [quater, setQuater] = useState(1);
+  const [timer, setTimer] = useState("15");
+
+  useEffect(() => {
+    const time = setTimeout(() => {
+      Number(timer) > 0
+        ? setTimer((Number(timer) - 1).toString())
+        : clearTimeout(time);
+    }, 1000);
+  }, [timer]);
 
   return (
     <div className="container">
@@ -19,7 +30,7 @@ function App() {
 
             <div className="home__score">{homeScore}</div>
           </div>
-          <div className="timer">00:03</div>
+          <div className="timer">{timer}</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
             <div className="away__score">{awayScore}</div>
